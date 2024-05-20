4c17b545ee79e3192647162dac824b9acb6d2ec2
Yavorskiy Vladislav <vl.yavorskiy01@gmail.com>
2024-05-20 20:54:02 +0300

    Готово

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  bodyEl.setAttribute("style", `background-color:${getRandomHexColor()}`);
+  spanEl.textContent = getRandomHexColor();
+}
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,48 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector("input");
+const btnElCreate = document.querySelector("button[data-create]");
+const btnElDestroy = document.querySelector("button[data-destroy]");
+const boxEl = document.querySelector("#boxes");
+
+btnElCreate.addEventListener("click", onBtnClickCreate);
+btnElDestroy.addEventListener("click", onBtnClickDestroy);
+
+function onBtnClickCreate() {
+  const amount = inputEl.value;
+
+  if (amount < 1 || amount > 100) {
+    return;
+  }
+  createBoxes(amount);
+  inputEl.value = "";
+}
+
+function createBoxes(amount) {
+  destroyBoxes();
+
+  const box = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+
+    box.push(div);
+    size += 10;
+  }
+
+  boxEl.append(...box);
+}
+
+function onBtnClickDestroy() {
+  destroyBoxes();
+}
+
+function destroyBoxes() {
+  boxEl.innerHTML = "";
+}
